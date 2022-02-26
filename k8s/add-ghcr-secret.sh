421e329f56d3246153853e35b65e1decb52114bc
KamushekDev <kamushek.stone@gmail.com>
2022-02-26 22:11:05 +0000

    added script

--- k8s/add-ghcr-secret.sh
+++ k8s/add-ghcr-secret.sh
@@ -5,7 +5,15 @@
 token=$1
 
 auth=$(echo -n KamushekDev:$1 | base64)
+# echo $auth
+authString='{"auths":{"ghcr.io":{"auth":"'
+authString+=$auth
+authString+='"}}}'
 
-echo '{"auths":{"ghcr.io":{"auth":"$auth"}}}' 
-| kubectl create secret generic dockerconfigjson-github-com --type=kubernetes.io/dockerconfigjson --from-file=.dockerconfigjson=/dev/stdin --namespace=crescore --save-config 
-| kubectl apply -f -
+# echo $authString
+kubectl create secret generic dockerconfigjson-github-com \
+	--type=kubernetes.io/dockerconfigjson \
+	--from-literal=.dockerconfigjson=$authString \
+	--namespace=crescore --save-config \
+	--dry-run=client -o yaml |
+kubectl apply -f -
