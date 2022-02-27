10a626c2a87f3f91c78a361a819d48536d3f6d64
KamushekDev <kamushek.stone@gmail.com>
2022-02-27 18:18:11 +0000

    Pass firebase admin secret #1

--- k8s/apply-all.sh
+++ k8s/apply-all.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-./apply-all.sh BUILD_VERSION
+#./apply-all.sh BUILD_VERSION
 
 version=$1
 
+echo "Building apps for version $version"
+
+touch current.yaml.temp
 files=$(find . -type f -name "*.yaml")
 for file in $files
 do
     echo $file
-    sed "s/{VERSION}/$version" "$file" > "_temp_.yaml"
-    echo $(cat _temp_.yaml)
+    sed "s/{VERSION}/$version/" $file > current.yaml.temp
+    # echo $(cat current.yaml.temp)
+    kubectl apply -f current.yaml.temp
 done
+
+rm current.yaml.temp
