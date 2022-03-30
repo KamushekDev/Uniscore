157211d9f73284ce010eaa695bafb53f0375d6cd
Ilin Kirill <kamushek.stone@gmail.com>
2022-03-30 09:54:07 +0300

    Bumped versions

--- dotnet/crescore-auth/src/CreScore.Auth/Program.cs
+++ dotnet/crescore-auth/src/CreScore.Auth/Program.cs
@@ -1,15 +1,9 @@
 using CreScore.Auth.Grpc;
 using CreScore.Auth.Infrastructure;
 using CreScore.Shared.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(80, o => o.Protocols = HttpProtocols.Http1AndHttp2);
-    options.ListenAnyIP(82, o => o.Protocols = HttpProtocols.Http2);
-    options.ListenAnyIP(84, o => o.Protocols = HttpProtocols.Http1);
-});
+builder.WebHost.ConfigureCustomKestrel(builder.Configuration);
 
 builder.Services.AddGrpc();
 
