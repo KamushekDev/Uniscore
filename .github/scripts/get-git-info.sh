564a5875ab126c6969e08329c155cf4a0a0ce54b
KamushekDev <kamushek.stone@gmail.com>
2022-04-06 00:19:57 +0100

    fixed typo

--- .github/scripts/get-git-info.sh
+++ .github/scripts/get-git-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $(git rev-parse --abbrev-ref HEAD)
-echo $(git tag
+echo $(git tag)
 echo "https://github.com/KamushekDev/CreScore/commit/"
 echo $(git rev-parse HEAD)
 echo $BUILD_VERSION
