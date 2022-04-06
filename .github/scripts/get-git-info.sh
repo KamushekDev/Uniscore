da5e0fe964adfa023a040180a802770940800039
KamushekDev <kamushek.stone@gmail.com>
2022-04-06 02:44:23 +0100

    Try #5

--- .github/scripts/get-git-info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo $(git rev-parse --abbrev-ref HEAD)
-echo $(git tag)
-echo "https://github.com/KamushekDev/CreScore/commit/"
-echo $(git rev-parse HEAD)
-echo $BUILD_VERSION
-
-{{github.ref_name}}
-{{github.sha}}
