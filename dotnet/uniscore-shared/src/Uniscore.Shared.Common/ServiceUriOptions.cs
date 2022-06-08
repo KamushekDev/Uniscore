5c261c63531bb4d5b5affd870c3a72845009669b
Ilin Kirill <kamushek.stone@gmail.com>
2022-06-08 22:25:06 +0300

    Changed uri options

--- dotnet/uniscore-shared/src/Uniscore.Shared.Common/ServiceUriOptions.cs
+++ dotnet/uniscore-shared/src/Uniscore.Shared.Common/ServiceUriOptions.cs
@@ -1,11 +1,11 @@
 ﻿namespace Uniscore.Shared.Common;
 
-public record ServiceUrl(string Host, int Port)
+public record ServiceUriOptions(string Host, int Port)
 {
     public const string SectionName = "ServiceUrls";
 
     private string? _uri;
     public string Uri => _uri ??= $"http://{Host}:{Port}";
 
-    public ServiceUrl() : this("localhost", 82) { }
+    public ServiceUriOptions() : this("localhost", 82) { }
 }
