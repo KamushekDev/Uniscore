48736d13ca0da99b49687054e7d3106d36e8b7cc
Ilin Kirill <kamushek.stone@gmail.com>
2022-06-22 00:51:13 +0300

    Changed auth

--- dotnet/uniscore-auth/src/Uniscore.Auth.Provider/Extensions/ServerCallContextExtensions.cs
+++ dotnet/uniscore-auth/src/Uniscore.Auth.Provider/Extensions/ServerCallContextExtensions.cs
@@ -8,7 +8,7 @@ public static class ServerCallContextExtensions
     public static ClaimsIdentity? GetClaims(this ServerCallContext context, bool shouldThrow = true)
     {
         var identity = context.GetHttpContext().User.Identities
-            .FirstOrDefault(x => x.AuthenticationType == Schemes.CreScoreScheme && x.IsAuthenticated);
+            .FirstOrDefault(x => x.AuthenticationType == Schemes.UniscoreScheme && x.IsAuthenticated);
         if (identity is null && shouldThrow)
             throw new Exception("User identity isn't presented in user data.");
 
