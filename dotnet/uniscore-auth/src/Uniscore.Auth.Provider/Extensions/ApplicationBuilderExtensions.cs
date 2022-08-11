63a264ecd5d3c8f341d7fca42ef1305abfec7e21
Ilin Kirill <kamushek.stone@gmail.com>
2022-08-12 00:46:38 +0300

    Changed auth

---
+++ dotnet/uniscore-auth/src/Uniscore.Auth.Provider/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Uniscore.Auth.Provider.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void UseUniscoreAuthorization(this IApplicationBuilder builder)
+    {
+        builder.UseAuthentication();
+        builder.UseAuthorization();
+    }
+}
