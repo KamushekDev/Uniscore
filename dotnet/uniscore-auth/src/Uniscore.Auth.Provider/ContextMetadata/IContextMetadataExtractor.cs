7d7f22a8af25dbc01048eebb199d3688a3208ce5
Ilin Kirill <kamushek.stone@gmail.com>
2022-08-12 05:04:14 +0300

    Updated auth

--- dotnet/uniscore-auth/src/Uniscore.Auth.Provider/ContextMetadata/IContextMetadataExtractor.cs
+++ dotnet/uniscore-auth/src/Uniscore.Auth.Provider/ContextMetadata/IContextMetadataExtractor.cs
@@ -1,6 +1,10 @@
-﻿namespace Uniscore.Auth.Provider.ContextMetadata;
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Http;
+
+namespace Uniscore.Auth.Provider.ContextMetadata;
 
 public interface IContextMetadataExtractor
 {
-    
+    string? GetUserId(ServerCallContext context);
+    string? GetUserId(HttpContext context);
 }
