ee9ee558fedc26361ced7ed3515d71bb06098877
Ilin Kirill <kamushek.stone@gmail.com>
2022-08-13 12:19:30 +0300

    df

--- dotnet/uniscore-users/src/Uniscore.Users.Infrastructure/ServiceCollectionExtensions.cs
+++ dotnet/uniscore-users/src/Uniscore.Users.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Uniscore.Auth.Provider.Extensions;
 using Uniscore.Auth.Provider.Options;
+using Uniscore.Auth.Provider.Requirements;
 using Uniscore.Shared.Hosting;
 using Uniscore.Users.Core.Users;
 using Uniscore.Users.Infrastructure.Firebase;
@@ -19,8 +23,34 @@ public static class ServiceCollectionExtensions
     {
         sc.AddUniscoreHealthChecks();
 
-        sc.AddAuth(configuration, environment);
+        //sc.AddAuth(configuration, environment);
 
+        sc.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "https://securetoken.google.com/unique-score";
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = "https://securetoken.google.com/unique-score",
+                    ValidateAudience = true,
+                    ValidAudience = "unique-score",
+                    ValidateLifetime = true
+                };
+            });
+        
+        sc.AddAuthorization((Action<AuthorizationOptions>) (configure =>
+        {
+            configure.AddPolicy("ValidUser", (Action<AuthorizationPolicyBuilder>) (policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.Requirements.Add((IAuthorizationRequirement) new ExistingUserRequirement());
+                // policy.AuthenticationSchemes.Add("uniscore-scheme");
+                policy.AuthenticationSchemes.Add("Bearer");
+            }));
+            configure.DefaultPolicy = configure.GetPolicy("ValidUser");
+        }));
+        
         sc.AddFirebase(configuration);
 
         sc.AddTransient<IUsersService, UsersService>();
