e9d1dbd3f9dd540bf2903fadd89f3d7ca7d1c84a
Ilin Kirill <kamushek.stone@gmail.com>
2022-08-16 05:16:21 +0300

    Added Uniscore.Shared.Auth

---
+++ dotnet/uniscore-shared/src/Uniscore.Shared.Auth/Options/UniscoreAuthorizationOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Uniscore.Shared.Auth.Options;
+
+public class UniscoreAuthorizationOptions
+{
+    public AuthorizationStatus Status { get; set; } = AuthorizationStatus.Enabled;
+}
+
+public enum AuthorizationStatus
+{
+    Enabled,
+    EnabledOnPod,
+    Disabled,
+    DisabledOnDevelopment,
+}
---
+++ dotnet/uniscore-shared/src/Uniscore.Shared.Auth/Requirements/ExistingUserRequirement.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Uniscore.Shared.Auth.Requirements;
+
+public class ExistingUserRequirement : IAuthorizationRequirement { }
