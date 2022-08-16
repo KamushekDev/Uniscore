6c1bf532d8414af8ba7bf40976f012ddfa164f13
Ilin Kirill <kamushek.stone@gmail.com>
2022-08-16 06:40:02 +0300

    Added script error forwarding

--- .github/scripts/build-all-projects.sh
+++ .github/scripts/build-all-projects.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'status_code=$?; end_command=$last_command; if [[ $status_code != 0 ]]; then echo "\"${end_command}\" command filed with exit code $status_code."; fi' EXIT
 
 solutions=$(find . -type f -name "*.sln")
 # echo "$solutions"
+
+BUILD_VERSION="2022.08.16-test"
 echo "build version: $BUILD_VERSION"
 for solution in $solutions
 do
