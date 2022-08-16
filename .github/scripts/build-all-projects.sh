b1a6d9c3b9092fe55d682a69f761287f33152d8e
Ilin Kirill <kamushek.stone@gmail.com>
2022-08-16 06:16:03 +0300

    Updated scripts

--- .github/scripts/build-all-projects.sh
+++ .github/scripts/build-all-projects.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 solutions=$(find . -type f -name "*.sln")
 # echo "$solutions"
 echo "build version: $BUILD_VERSION"
