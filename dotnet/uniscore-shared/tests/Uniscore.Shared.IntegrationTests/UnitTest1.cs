0bb62378d14b1263300280720ecc0c507e3b653c
Ilin Kirill <kamushek.stone@gmail.com>
2022-08-20 20:25:08 +0300

    fdg
    sdfdfgf

---
+++ dotnet/uniscore-shared/tests/Uniscore.Shared.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Uniscore.Shared.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact(DisplayName = "Супер крутой интеграционный тест")]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
